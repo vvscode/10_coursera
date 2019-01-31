35ad698962845592fa35572bcf51900e99d828f5
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-31 17:57:13 +0400

    Fix notices

--- coursera.py
+++ coursera.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 from collections import namedtuple
 from openpyxl import Workbook
+from dateutil.parser import parse
 import argparse
 import os
 import re
 import requests
 import sys
 import xml.etree.ElementTree as ET
+import json
 
 course_info = namedtuple("course_info", [
     "title",
@@ -18,76 +20,103 @@ course_info = namedtuple("course_info", [
 ])
 
 
-def get_text_from_url(url):
+def fetch(url):
     return requests.get(url).text
 
 
 def extract_courses_links_from_text(text):
-    loc_elements = ET.fromstring(text).findall(".//{http://www.sitemaps.org/schemas/sitemap/0.9}loc")
+    loc_elements = ET.fromstring(text).findall(
+        ".//{http://www.sitemaps.org/schemas/sitemap/0.9}loc")
     return map(lambda item: item.text, loc_elements)
 
 
 def get_courses_list():
-    xml_string = get_text_from_url("https://www.coursera.org/sitemap~www~courses.xml")
+    xml_string = fetch("https://www.coursera.org/sitemap~www~courses.xml")
     return list(extract_courses_links_from_text(xml_string))
 
 
-def get_course_weeks_duration(page, page_html):
-    week_begin_marker = "material.weeks."
-    number_start = page_html.rfind(week_begin_marker) + len(week_begin_marker)
-    number_end = page_html.find('"', number_start)
-    number_string = page_html[number_start:number_end].strip()
-    # numbers starts from 0
-    return int(number_string) + 1
+def get_graphql_data(soup):
+    graphql_data_container = soup.select_one(
+        'script[type="application/ld+json"]')
+    graphql_data = json.loads(graphql_data_container.get_text())
+    return graphql_data
 
 
-def get_course_start_date_string(page, page_html):
-    return re.search(r'validFrom":"(.+?)"', page_html).group(1)
-
-
-def get_course_lang(page, page_html):
-    return page.select_one(".ProductGlance > :last-of-type h4").get_text()
-
-
-def get_course_rating(page, page_html):
+def get_course_weeks_duration(soup, page_html, graphql_data):
+    week_days = 7
+    if not graphql_data:
+        return
     try:
-        return page.select_one(".AboutCourse [class*=StarRating] ~ span").get_text()
+        start_date = parse(graphql_data["@graph"]
+                           [2]["hasCourseInstance"]["startDate"])
+        end_date = parse(graphql_data["@graph"][2]
+                         ["hasCourseInstance"]["endDate"])
+    except KeyError:
+        return None
+
+    days = (end_date - start_date).days
+    return days / week_days
+
+
+def get_course_start_date_string(soup, page_html, graphql_data):
+    try:
+        return graphql_data["@graph"][2]["hasCourseInstance"]["startDate"]
+    except KeyError:
+        return None
+
+
+def get_course_lang(soup, page_html, graphql_data):
+    if not graphql_data:
+        return soup.select_one(".ProductGlance > :last-of-type h4").get_text()
+
+    return graphql_data["@graph"][2]["inLanguage"]
+
+
+def get_course_rating(soup, page_html, graphql_data):
+    if graphql_data:
+        return graphql_data["@graph"][1]["aggregateRating"]["ratingValue"]
+
+    try:
+        rating_el = soup.select_one(".AboutCourse [class*=StarRating] ~ span")
+        return rating_el.get_text()
     except AttributeError:
-        return "Unknown"
+        return None
 
 
-def get_course_info(link):
-    page_html = get_text_from_url(link)
-    page = BeautifulSoup(page_html, "html.parser")
+def get_course_info(link, page_html):
+    soup = BeautifulSoup(page_html, "html.parser")
+    graphql_data = get_graphql_data(soup)
     return course_info(
-        title=page.select_one(".BannerTitle h1").get_text(),
+        title=soup.select_one(".BannerTitle h1").get_text(),
         link=link,
-        lang=get_course_lang(page, page_html),
-        start_date=get_course_start_date_string(page, page_html),
-        duration_weeks=get_course_weeks_duration(page, page_html),
-        rating=get_course_rating(page, page_html)
+        lang=get_course_lang(soup, page_html, graphql_data),
+        start_date=get_course_start_date_string(soup, page_html, graphql_data),
+        duration_weeks=get_course_weeks_duration(
+            soup, page_html, graphql_data),
+        rating=get_course_rating(soup, page_html, graphql_data)
     )
 
 
-def output_courses_info_to_xlsx(filepath, courses):
-    if not courses:
-        return
-
+def put_courses_to_workbook(workbook, courses):
     workbook = Workbook()
     worksheet = workbook.active
 
-    fileds = courses[0]._fields
-    worksheet.append(fileds)
+    courses = list(courses)
+
+    fields = courses[0]._fields
+    worksheet.append(fields)
 
     for line in courses:
-        worksheet.append(line)
-
-    workbook.save(filepath)
+        worksheet.append(
+            list(map(lambda x: x or 'Unknown', line))
+        )
 
 
 def get_params():
     parser = argparse.ArgumentParser(
-        description="Coursera parser - it will parse information about the courses and put it to the file"
+        description="""
+            Coursera parser - it will parse information about the courses and put it to the file
+        """
     )
     parser.add_argument("output", help="Path to result file")
     parser.add_argument("--limit", default=20, type=int, help="Limit courses")
@@ -104,17 +133,23 @@ def get_params():
 if __name__ == "__main__":
     args = get_params()
 
-    print("We're going collect information about {} course(s). Please wait".format(args.limit))
+    print("We're going collect information about {} course(s). Please wait".format(
+        args.limit))
 
     courses_list = get_courses_list()[:args.limit]
 
     if not courses_list:
         sys.exit("No courses found")
 
-    courses_info = list(map(get_course_info, courses_list))
+    courses_info = map(
+        lambda link: get_course_info(link, fetch(link)),
+        courses_list
+    )
 
+    workbook = Workbook()
+    put_courses_to_workbook(workbook, courses_info)
     try:
-        output_courses_info_to_xlsx(args.output, courses_info)
+        workbook.save(args.output)
     except FileNotFoundError:
         sys.exit("Can't write to file")
 
